5ad6f5bcef225c70426689fd186f9dd4a0c3d702
Mor <mortogo321@gmail.com>
2022-09-13 08:59:39 +0700

    Allow KeyConditionExpression when model has composite key and partition key only is passed in where

--- src/ConditionAnalyzer/Analyzer.php
+++ src/ConditionAnalyzer/Analyzer.php
@@ -71,8 +71,16 @@ class Analyzer
             return false;
         }
 
+        if (count($this->conditions) !== count($this->model->getKeyNames())) {
+            return false;
+        }
+
         foreach ($this->conditions as $condition) {
             if (Arr::get($condition, 'type') !== ComparisonOperator::EQ) {
+                return false;
+            }
+
+            if (array_search(Arr::get($condition, 'column'), $this->model->getKeyNames()) === false) {
                 return false;
             }
         }
@@ -202,7 +210,7 @@ class Analyzer
         $hashConditionType = $this->getCondition($hash)['type'] ?? null;
         $validQueryOp = ComparisonOperator::isValidQueryDynamoDbOperator($hashConditionType);
 
-        if ($validQueryOp && $range) {
+        if ($validQueryOp && $range && $this->getCondition($range)) {
             $rangeConditionType = $this->getCondition($range)['type'] ?? null;
             $validQueryOp = ComparisonOperator::isValidQueryDynamoDbOperator(
                 $rangeConditionType,
